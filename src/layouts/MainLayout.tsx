3deb6479e012f5b42449e9dce3a70840136eb9aa
小李zhen <986329665@qq.com>
2023-05-05 11:04:29 +0800

    创建layouts

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const MainLayout: FC = () => {
+    return <>MainLayout</>
+}
+
+export default MainLayout
---
+++ src/layouts/ManageLayout.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const ManageLayout: FC = () => {
+    return <>ManageLayout</>
+}
+
+export default ManageLayout
